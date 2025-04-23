090e9476ccb7af770f262891b4a05e95399b1367
marifer91 <maria.cueva.sistemas@gmail.com>
2025-04-23 09:35:00 +0000

    23-04 last today

--- details.php
+++ details.php
@@ -15,7 +15,7 @@ include "components/head.php";
     
        <?php include "components/nav.php"; ?> 
     
-    
+<div class= "detailscontainer1">
             <div class = "containerdetails" >
             <div class = "shoesdetails">
               <img class= "shoesimagen" src="images/shoess.png" alt="">
@@ -55,6 +55,127 @@ include "components/head.php";
                 </div>
             </div>
          </div>
+
+         <div class = "containerdetails" >
+            <div class = "shoesdetails">
+              <img class= "shoesimagen" src="images/shoess.png" alt="">
+                <div class ="slideshow-buttons">
+                    <span></span>
+                    <span class = "focus1"></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <b><p class="pick">choose size</p></b>
+                <div class ="sizes">
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span class ="focus1"> 8 </span>
+                <span>9</span>
+                <span>10</span>
+                <span>11</span>
+                <span>12</span></br></br>
+                </div>
+            </div>
+            <div class = "product2">
+                <b><p > &nbsp Women's Running Shoe</p> </b>
+                <h1> &nbsp Nike Epic React Flyknit</h1>
+                <h2> &nbsp $ 150 AUS</h2>
+                  <p class= "description">
+                    The Nike Epic React Flyknit foam cushioning is resonsive yet light-weight, durable yet soft.
+                    This creates a sensation that not only enhances the feeling of moving forward.
+                  </p>
+                  
+                <div class="buttonshere">
+                  
+                    <button class="add">Add to Cart </button></br>
+                    <input class= "input" type="text" id= "subject" name="subject" placeholder="Quantity">
+                    <button class = "like"> ❤︎ </button>
+                  
+                </div>
+            </div>
+         </div>
+
+         <div class = "containerdetails" >
+            <div class = "shoesdetails">
+              <img class= "shoesimagen" src="images/shoess.png" alt="">
+                <div class ="slideshow-buttons">
+                    <span></span>
+                    <span class = "focus1"></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <b><p class="pick">choose size</p></b>
+                <div class ="sizes">
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span class ="focus1"> 8 </span>
+                <span>9</span>
+                <span>10</span>
+                <span>11</span>
+                <span>12</span></br></br>
+                </div>
+            </div>
+            <div class = "product2">
+                <b><p > &nbsp Women's Running Shoe</p> </b>
+                <h1> &nbsp Nike Epic React Flyknit</h1>
+                <h2> &nbsp $ 150 AUS</h2>
+                  <p class= "description">
+                    The Nike Epic React Flyknit foam cushioning is resonsive yet light-weight, durable yet soft.
+                    This creates a sensation that not only enhances the feeling of moving forward.
+                  </p>
+                  
+                <div class="buttonshere">
+                  
+                    <button class="add">Add to Cart </button></br>
+                    <input class= "input" type="text" id= "subject" name="subject" placeholder="Quantity">
+                    <button class = "like"> ❤︎ </button>
+                  
+                </div>
+            </div>
+         </div>
+
+         <div class = "containerdetails" >
+            <div class = "shoesdetails">
+              <img class= "shoesimagen" src="images/shoess.png" alt="">
+                <div class ="slideshow-buttons">
+                    <span></span>
+                    <span class = "focus1"></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <b><p class="pick">choose size</p></b>
+                <div class ="sizes">
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span class ="focus1"> 8 </span>
+                <span>9</span>
+                <span>10</span>
+                <span>11</span>
+                <span>12</span></br></br>
+                </div>
+            </div>
+            <div class = "product2">
+                <b><p > &nbsp Women's Running Shoe</p> </b>
+                <h1> &nbsp Nike Epic React Flyknit</h1>
+                <h2> &nbsp $ 150 AUS</h2>
+                  <p class= "description">
+                    The Nike Epic React Flyknit foam cushioning is resonsive yet light-weight, durable yet soft.
+                    This creates a sensation that not only enhances the feeling of moving forward.
+                  </p>
+                  
+                <div class="buttonshere">
+                  
+                    <button class="add">Add to Cart </button></br>
+                    <input class= "input" type="text" id= "subject" name="subject" placeholder="Quantity">
+                    <button class = "like"> ❤︎ </button>
+                  
+                </div>
+            </div>
+         </div>
+</div>
        
         
 </body>
