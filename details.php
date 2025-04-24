1bf5ae25c954aa7e952c609f760afd13095b6a6e
marifer91 <maria.cueva.sistemas@gmail.com>
2025-04-24 07:36:19 +0000

    24-04

--- details.php
+++ details.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 
@@ -46,17 +47,17 @@ include "components/head.php";
                     This creates a sensation that not only enhances the feeling of moving forward.
                   </p>
                   
-                <div class="buttonshere">
+              <div class="buttonshere">
                   
-                    <button class="add">Add to Cart </button></br>
-                    <input class= "input" type="text" id= "subject" name="subject" placeholder="Quantity">
-                    <button class = "like"> ❤︎ </button>
-                  
-                </div>
+                  <input class= "input" type="text" id= "subject" name="subject" placeholder="Unit">
+                  <button class="add">Add to Cart </button>
+                  <button class = "like"> ❤ </button>
+                
+              </div>
             </div>
-         </div>
+          </div>
 
-         <div class = "containerdetails" >
+          <div class = "containerdetails" >
             <div class = "shoesdetails">
               <img class= "shoesimagen" src="images/shoess.png" alt="">
                 <div class ="slideshow-buttons">
@@ -86,95 +87,20 @@ include "components/head.php";
                     This creates a sensation that not only enhances the feeling of moving forward.
                   </p>
                   
-                <div class="buttonshere">
+              <div class="buttonshere">
                   
-                    <button class="add">Add to Cart </button></br>
-                    <input class= "input" type="text" id= "subject" name="subject" placeholder="Quantity">
-                    <button class = "like"> ❤︎ </button>
-                  
-                </div>
+                  <input class= "input" type="text" id= "subject" name="subject" placeholder="Unit">
+                  <button class="add">Add to Cart </button>
+                  <button class = "like"> ❤ </button>
+                
+              </div>
             </div>
-         </div>
+          </div>
 
-         <div class = "containerdetails" >
-            <div class = "shoesdetails">
-              <img class= "shoesimagen" src="images/shoess.png" alt="">
-                <div class ="slideshow-buttons">
-                    <span></span>
-                    <span class = "focus1"></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <b><p class="pick">choose size</p></b>
-                <div class ="sizes">
-                <span>5</span>
-                <span>6</span>
-                <span>7</span>
-                <span class ="focus1"> 8 </span>
-                <span>9</span>
-                <span>10</span>
-                <span>11</span>
-                <span>12</span></br></br>
-                </div>
-            </div>
-            <div class = "product2">
-                <b><p > &nbsp Women's Running Shoe</p> </b>
-                <h1> &nbsp Nike Epic React Flyknit</h1>
-                <h2> &nbsp $ 150 AUS</h2>
-                  <p class= "description">
-                    The Nike Epic React Flyknit foam cushioning is resonsive yet light-weight, durable yet soft.
-                    This creates a sensation that not only enhances the feeling of moving forward.
-                  </p>
-                  
-                <div class="buttonshere">
-                  
-                    <button class="add">Add to Cart </button></br>
-                    <input class= "input" type="text" id= "subject" name="subject" placeholder="Quantity">
-                    <button class = "like"> ❤︎ </button>
-                  
-                </div>
-            </div>
-         </div>
 
-         <div class = "containerdetails" >
-            <div class = "shoesdetails">
-              <img class= "shoesimagen" src="images/shoess.png" alt="">
-                <div class ="slideshow-buttons">
-                    <span></span>
-                    <span class = "focus1"></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <b><p class="pick">choose size</p></b>
-                <div class ="sizes">
-                <span>5</span>
-                <span>6</span>
-                <span>7</span>
-                <span class ="focus1"> 8 </span>
-                <span>9</span>
-                <span>10</span>
-                <span>11</span>
-                <span>12</span></br></br>
-                </div>
-            </div>
-            <div class = "product2">
-                <b><p > &nbsp Women's Running Shoe</p> </b>
-                <h1> &nbsp Nike Epic React Flyknit</h1>
-                <h2> &nbsp $ 150 AUS</h2>
-                  <p class= "description">
-                    The Nike Epic React Flyknit foam cushioning is resonsive yet light-weight, durable yet soft.
-                    This creates a sensation that not only enhances the feeling of moving forward.
-                  </p>
-                  
-                <div class="buttonshere">
-                  
-                    <button class="add">Add to Cart </button></br>
-                    <input class= "input" type="text" id= "subject" name="subject" placeholder="Quantity">
-                    <button class = "like"> ❤︎ </button>
-                  
-                </div>
-            </div>
-         </div>
+
+
+
 </div>
        
         
---
+++ details1.php
@@ -0,0 +1,105 @@
+<?php
+include 'src/database.php';
+// get the products data from data base
+$query = "
+SELECT 
+id,
+name,
+category,
+description,
+price,
+image
+FROM products
+";
+$statement = $connection -> prepare($query);
+$statement -> execute();
+$result = $statement -> get_result();
+$products = array();
+while( $row = $result -> fetch_assoc()){
+      array_push ( $products,$row );
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<?php
+//include head section
+include "components/head.php";
+?>
+
+
+
+<body>
+
+      <?php include "components/header.php"; ?> 
+
+    
+       <?php include "components/nav.php"; ?> 
+    
+    <main>
+    
+       <?php
+	     //output products 
+	     foreach ($products as $p){
+	        $image = $p["image"];
+	        $name = $p["name"];
+	        $description = $p["description"];
+	        $price = $p["price"];
+	     echo "
+          <div class= 'detailscontainer1'>
+            <div class = 'shoesdetails'>
+              <img class= 'shoesimagen' src='images/$image' alt=''>
+                <div class ='slideshow-buttons'>
+                    <span></span>
+                    <span class = 'focus1'></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <b><p class='pick'>choose size</p></b>
+                <div class ='sizes'>
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span class ='focus1'> 8 </span>
+                <span>9</span>
+                <span>10</span>
+                <span>11</span>
+                <span>12</span></br></br>
+                </div>
+            </div>
+            <div class = 'product2'>
+                <b><p > $name</p> </b>
+                <h1> &nbsp Nike Epic React Flyknit</h1>
+                <h2> &nbsp $price</h2>
+                  <p class= 'description'>
+                    $description
+                  </p>
+                  
+              <div class='buttonshere'>
+                  
+                  <input class= 'input' type='text' id= 'subject' name='subject' placeholder='Unit'>
+                  <button class='add'>Add to Cart </button>
+                  <button class = 'like'> ❤ </button>
+                
+              </div>
+            </div>
+          </div>
+         </div>
+        
+	 ";
+        }
+	 ?>
+    
+
+     
+    </main>
+
+       
+        
+</body>
+
+<?php include "components/footer.php"?>
+
+</html>
