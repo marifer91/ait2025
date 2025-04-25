a43c64e7e2f58fa82fdccc66379b5a79b80293b0
marifer91 <maria.cueva.sistemas@gmail.com>
2025-04-25 02:05:03 +0000

    25-04 last

--- details.php
+++ details.php
@@ -1,3 +1,25 @@
+<?php
+include 'src/database.php';
+// get the products data from data base
+$query = "
+SELECT 
+id,
+name,
+category,
+description,
+price,
+image
+FROM products
+";
+$statement = $connection->prepare($query);
+$statement->execute();
+$result = $statement->get_result();
+$products = array();
+while ($row = $result->fetch_assoc()) {
+  array_push($products, $row);
+}
+?>
+
 
 <!DOCTYPE html>
 <html>
@@ -11,101 +33,74 @@ include "components/head.php";
 
 <body>
 
-      <?php include "components/header.php"; ?> 
-
-    
-       <?php include "components/nav.php"; ?> 
-    
-       <div class= "detailscontainer1">
-            <div class = "containerdetails" >
-            <div class = "shoesdetails">
-              <img class= "shoesimagen" src="images/shoess.png" alt="">
-                <div class ="slideshow-buttons">
-                    <span></span>
-                    <span class = "focus1"></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <b><p class="pick">choose size</p></b>
-                <div class ="sizes">
-                <span>5</span>
-                <span>6</span>
-                <span>7</span>
-                <span class ="focus1"> 8 </span>
-                <span>9</span>
-                <span>10</span>
-                <span>11</span>
-                <span>12</span></br></br>
-                </div>
-            </div>
-            <div class = "product2">
-                <b><p > &nbsp Women's Running Shoe</p> </b>
-                <h1> &nbsp Nike Epic React Flyknit</h1>
-                <h2> &nbsp $ 150 AUS</h2>
-                  <p class= "description">
-                    The Nike Epic React Flyknit foam cushioning is resonsive yet light-weight, durable yet soft.
-                    This creates a sensation that not only enhances the feeling of moving forward.
-                  </p>
-                  
-              <div class="buttonshere">
-                  
-                  <input class= "input" type="text" id= "subject" name="subject" placeholder="Unit">
-                  <button class="add">Add to Cart </button>
-                  <button class = "like"> ❤ </button>
-                
-              </div>
-            </div>
-          </div>
-
-          <div class = "containerdetails" >
-            <div class = "shoesdetails">
-              <img class= "shoesimagen" src="images/shoess.png" alt="">
-                <div class ="slideshow-buttons">
-                    <span></span>
-                    <span class = "focus1"></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <b><p class="pick">choose size</p></b>
-                <div class ="sizes">
-                <span>5</span>
-                <span>6</span>
-                <span>7</span>
-                <span class ="focus1"> 8 </span>
-                <span>9</span>
-                <span>10</span>
-                <span>11</span>
-                <span>12</span></br></br>
-                </div>
-            </div>
-            <div class = "product2">
-                <b><p > &nbsp Women's Running Shoe</p> </b>
-                <h1> &nbsp Nike Epic React Flyknit</h1>
-                <h2> &nbsp $ 150 AUS</h2>
-                  <p class= "description">
-                    The Nike Epic React Flyknit foam cushioning is resonsive yet light-weight, durable yet soft.
-                    This creates a sensation that not only enhances the feeling of moving forward.
-                  </p>
-                  
-              <div class="buttonshere">
-                  
-                  <input class= "input" type="text" id= "subject" name="subject" placeholder="Unit">
-                  <button class="add">Add to Cart </button>
-                  <button class = "like"> ❤ </button>
-                
-              </div>
-            </div>
-          </div>
+  <?php include "components/header.php"; ?>
 
 
+  <?php include "components/nav.php"; ?>
 
-
-
-</div>
-       
+  
+    <div class = "detailscontainer11">
+      <?php
+      //output products 
+      foreach ($products as $p) {
+        $image = $p["image"];
+        $name = $p["name"];
+        $description = $p["description"];
+        $price = $p["price"];
+        echo "
         
+           <div class = 'card1' >
+            <div class = 'shoesdetails'>
+              <img class= 'shoesimagen' src='images/$image' alt=''>
+                <div class ='slideshow-buttons'>
+                    <span></span>
+                    <span class = 'focus1'></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <b><p class='pick'>choose size</p></b>
+                <div class ='sizes'>
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span class ='focus1'> 8 </span>
+                <span>9</span>
+                <span>10</span>
+                <span>11</span>
+                <span>12</span></br></br>
+                </div>
+            </div>
+            <div class = 'product2'>
+                <b><p > $name</p> </b>
+                <h1> &nbsp Nike Epic React Flyknit</h1>
+                <h2> &nbsp $price</h2>
+                  <p class= 'description'>
+                    $description
+                  </p>
+                  
+              <div class='buttonshere'>
+                  
+                  <input class= 'input' type='text' id= 'subject' name='subject' placeholder='Unit'>
+                  <button class='add'>Add to Cart </button>
+                  <button class = 'like'> ❤ </button>
+                
+              </div>
+            </div>
+          </div>
+        
+        
+	     ";
+         }
+      ?>
+
+    </div>
+
+  
+
+
+
 </body>
 
-<?php include "components/footer.php"?>
+<?php include "components/footer.php" ?>
 
 </html>
---
+++ details2.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html>
+
+<?php
+//include head section
+include "components/head.php";
+?>
+
+
+
+<body>
+
+      <?php include "components/header.php"; ?> 
+
+    
+       <?php include "components/nav.php"; ?> 
+    
+       <div class= "detailscontainer1">
+            <div class = "containerdetails" >
+            <div class = "shoesdetails">
+              <img class= "shoesimagen" src="images/shoess.png" alt="">
+                <div class ="slideshow-buttons">
+                    <span></span>
+                    <span class = "focus1"></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <b><p class="pick">choose size</p></b>
+                <div class ="sizes">
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span class ="focus1"> 8 </span>
+                <span>9</span>
+                <span>10</span>
+                <span>11</span>
+                <span>12</span></br></br>
+                </div>
+            </div>
+            <div class = "product2">
+                <b><p > &nbsp Women's Running Shoe</p> </b>
+                <h1> &nbsp Nike Epic React Flyknit</h1>
+                <h2> &nbsp $ 150 AUS</h2>
+                  <p class= "description">
+                    The Nike Epic React Flyknit foam cushioning is resonsive yet light-weight, durable yet soft.
+                    This creates a sensation that not only enhances the feeling of moving forward.
+                  </p>
+                  
+              <div class="buttonshere">
+                  
+                  <input class= "input" type="text" id= "subject" name="subject" placeholder="Unit">
+                  <button class="add">Add to Cart </button>
+                  <button class = "like"> ❤ </button>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class = "containerdetails" >
+            <div class = "shoesdetails">
+              <img class= "shoesimagen" src="images/shoess.png" alt="">
+                <div class ="slideshow-buttons">
+                    <span></span>
+                    <span class = "focus1"></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <b><p class="pick">choose size</p></b>
+                <div class ="sizes">
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span class ="focus1"> 8 </span>
+                <span>9</span>
+                <span>10</span>
+                <span>11</span>
+                <span>12</span></br></br>
+                </div>
+            </div>
+            <div class = "product2">
+                <b><p > &nbsp Women's Running Shoe</p> </b>
+                <h1> &nbsp Nike Epic React Flyknit</h1>
+                <h2> &nbsp $ 150 AUS</h2>
+                  <p class= "description">
+                    The Nike Epic React Flyknit foam cushioning is resonsive yet light-weight, durable yet soft.
+                    This creates a sensation that not only enhances the feeling of moving forward.
+                  </p>
+                  
+              <div class="buttonshere">
+                  
+                  <input class= "input" type="text" id= "subject" name="subject" placeholder="Unit">
+                  <button class="add">Add to Cart </button>
+                  <button class = "like"> ❤ </button>
+                
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+</div>
+       
+        
+</body>
+
+<?php include "components/footer.php"?>
+
+</html>
